0284dd66f023521fe858b0d7fae723114b6a2a0f
qzart45hi <qwert45hi@yandex.ru>
2022-01-06 22:18:42 +0300

    feat(wesite): initial setup

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div> 
+    HI!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
